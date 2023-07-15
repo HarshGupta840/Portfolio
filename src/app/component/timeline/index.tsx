e592333c93605f19054e8568ddba5c930383fb22
Harsh Gupta <harsh840gupta@gmail.com>
2023-07-15 23:52:13 +0530

    some animation trials

--- src/app/component/timeline/index.tsx
+++ src/app/component/timeline/index.tsx
@@ -1,37 +1,8 @@
 "use client";
 import React, { useEffect, useRef } from "react";
-// import { gsap } from "gsap";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-// gsap.registerPlugin(ScrollTrigger);
-
 type Props = {};
 
 const Timeline = ({}: Props) => {
-  // const timelineRef = useRef<HTMLDivElement>(null);
-  // useEffect(() => {
-  //   gsap.from(timelineRef.current!.querySelectorAll(".container.left"), {
-  //     opacity: 0,
-  //     x: -100,
-  //     duration: 1,
-  //     stagger: 0.2,
-  //     scrollTrigger: {
-  //       trigger: timelineRef.current!,
-  //       start: "top 80%",
-  //     },
-  //   });
-
-  //   gsap.from(timelineRef.current!.querySelectorAll(".container.right"), {
-  //     opacity: 0,
-  //     x: 100,
-  //     duration: 1,
-  //     stagger: 0.2,
-  //     scrollTrigger: {
-  //       trigger: timelineRef.current!,
-  //       start: "top 80%",
-  //     },
-  //   });
-  // }, []);
   return (
     <section className="py-16 lg:py-20">
       <div className="max-w-xl mx-auto"></div>
@@ -87,7 +58,7 @@ const Timeline = ({}: Props) => {
                         viewBox="0 0 16 16"
                       >
                         <path
-                          fill-rule="evenodd"
+                          fillRule="evenodd"
                           d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022zM6 8.694 1 10.36V15h5V8.694zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15z"
                         ></path>
                         <path d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z"></path>
